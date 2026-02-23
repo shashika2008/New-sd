20de32800f13ede7e06c15f6e4e74feaf1aa79de
Mr shashika dilshan <dmritasudarsani@gmail.com>
2026-02-23 13:38:56 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "suho~7RlHlSTT#WdJg5kL4mLQQXD22ZrixMPVdwwen73u-e3tv_goDP3k",
+SESSION_ID: process.env.SESSION_ID || "Pc9XjbwL#z3cSQsnIvcmfupZUS88VfL94LOZeBFSra24_vTgGoDI",
 MONGODB: process.env.MONGODB || "mongodb://mongo:FbAOzhLzBjBIoyVZegPugoscYfHbHsef@hopper.proxy.rlwy.net:35489",
 OPENAI_API_KEY: process.env.OPENAI_API_KEY || "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"
 };
